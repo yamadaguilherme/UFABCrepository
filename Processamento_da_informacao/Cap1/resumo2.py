80dcfe08e42c320b3f2da34ed7e46a2fb8bd9957
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2024-10-26 09:42:10 -0300

    Update resumo2.py

--- Processamento_da_informacao/Cap1/resumo2.py
+++ Processamento_da_informacao/Cap1/resumo2.py
@@ -6,6 +6,6 @@
 
 #Assim, se for lido o valor 65 para C, o resultado a ser exibido deve ser "65 graus Celsius corresponde a 149.0 graus Fahrenheit", se a variável C linda for igual a 65. 
 
-C = int(input()) # ATENÇÃO NESTA INSTRUÇÃO!
+C = int(input()) 
 F = C * 9/5 + 32
 print(C,"graus Celsius corresponde a", F, "graus Fahrenheit")
