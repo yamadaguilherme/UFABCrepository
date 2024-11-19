7c099d0b3234900a87e8d1a9ca74c37554efbca3
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2024-11-19 16:12:11 -0300

    Delete Processamento_da_informacao/Cap1/resumo3.py

--- Processamento_da_informacao/Cap1/resumo3.py
+++
@@ -1,8 +0,0 @@
-#Escreva instruções para calulcar o delta utilizando a função "delta()", onde a função é dada por b * b - 4 * a * c, calcule para
-# os valores de 5x^2 – 2x + 4
-
-def delta( a, b, c ):
-  d = b * b - 4 * a * c
-  return d
-valor = delta( 5, -2, 4)
-print("O delta de 5x^2 – 2x + 4 é %.1f" % valor)
