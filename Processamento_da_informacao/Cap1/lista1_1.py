3cdfb8b8830fe37f4c691220c0df64635a2f2afc
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2024-11-19 15:39:19 -0300

    Update lista1_1.py

--- Processamento_da_informacao/Cap1/lista1_1.py
+++ Processamento_da_informacao/Cap1/lista1_1.py
@@ -1,17 +1,17 @@
 '''
-Considere as instru ̧c ̃oes abaixo:
+Considere as instrucoes abaixo:
    inteiro distancia = 20, KM Inicial = 19.76 , KM Final
    ler (KM Final )
    distancia = KM Final - KM Inicial
    escreva ( distancia )
-Se KM Final for uma vari ́avel de entrada, qual  ́e a distˆancia impressa (considerando o valor lido em KM Final)? Para
-responder, fa ̧ca um programa que reproduza as instru ̧c ̃oes acima. Aqui um exemplo de como seria entrada e a sa ́ıda desse
+Se KM Final for uma variavel de entrada, qual  ́e a distancia impressa (considerando o valor lido em KM Final)? Para
+responder, faca um programa que reproduza as instrucoes acima. Aqui um exemplo de como seria entrada e a saıda desse
 programa:
 Entrada:
 201.46
 Saıda:
 distancia = 181.70
-Atencao: a sa ́ıda deve ter exatamente a mesma formatação desse exemplo acima.
+Atencao: a saıda deve ter exatamente a mesma formatação desse exemplo acima.
 '''
 
 KM_inicial = 19.76
