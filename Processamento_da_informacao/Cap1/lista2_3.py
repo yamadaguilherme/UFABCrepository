ef637263e6e90d0f377784e5abf7485dde1226ba
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2024-11-19 16:08:45 -0300

    Update lista2_3.py

--- Processamento_da_informacao/Cap1/lista2_3.py
+++ Processamento_da_informacao/Cap1/lista2_3.py
@@ -1,3 +1,16 @@
+'''
+A distancia Chebyshev entre dois pontos p1 e p2 no plano cartesiano e dado por
+max{| x2 − x1 |, | y2 − y1 |}.
+Suponha que e dado um ponto p1=(−7, 1). Agora faca um programa que:
+• Leia a partir do teclado os valores das coordenadas de um outro ponto p2 e passe-os para uma funcao, que chamaremos
+de calcula distancia Chebyshev, a qual devera calcular e retornar (via comando return) a distancia Chebyshev
+entre p1 e p2, de acordo com a formula descrita acima.
+• Imprima o valor da distancia calculada. Importante: a(s) instrucao(coes) para impressao deve(m) estar fora da funcao
+calcula distancia Chebyshev.
+Atencao: o uso de funcoes e obrigatorio, a sua nao utilizacao implica em anulacao da questao.
+Obs.: imprima a saıda com duas casas decimais.
+'''
+
 def entrada():
     global p1x, p1y, p2x, p2y
     p1x = -7
