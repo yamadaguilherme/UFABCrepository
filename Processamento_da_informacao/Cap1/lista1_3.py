40144f706cf19859ecc42c12aa168a8113e821d7
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2024-11-19 15:41:08 -0300

    Update lista1_3.py

--- Processamento_da_informacao/Cap1/lista1_3.py
+++ Processamento_da_informacao/Cap1/lista1_3.py
@@ -1,12 +1,14 @@
-#Escreva um programa que ajude uma loja a fazer uma venda cujo pagamento  ́e parcelado. Assuma que a loja divide o
-#pre ̧co em 21 parcelas iguais e sem quaisquer acr ́escimos (juros, multas, etc). Fa ̧ca programa que:
-#• leia do teclado o pre ̧co do produto;
-#• imprima quanto custar ́a cada parcela (arredondando com duas casas decimais).
-#Veja aqui um exemplo:
-#Entrada:
-#804.36
-#Sa ́ıda:
-#38.30
+'''
+Escreva um programa que ajude uma loja a fazer uma venda cujo pagamento  ́e parcelado. Assuma que a loja divide o
+preco em 21 parcelas iguais e sem quaisquer acrescimos (juros, multas, etc). Faca programa que:
+• leia do teclado o preco do produto;
+• imprima quanto custar ́a cada parcela (arredondando com duas casas decimais).
+Veja aqui um exemplo:
+Entrada:
+804.36
+Sa ́ıda:
+38.30
+'''
 
 produto= float(input())
 parc = produto/21
