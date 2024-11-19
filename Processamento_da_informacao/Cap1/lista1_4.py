2f0f344d91a37f5c571b666ec554c4d5011c1ae3
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2024-11-19 15:41:53 -0300

    Update lista1_4.py

--- Processamento_da_informacao/Cap1/lista1_4.py
+++ Processamento_da_informacao/Cap1/lista1_4.py
@@ -1,12 +1,13 @@
-'''Fa ̧ca um programa contendo instru ̧c ̃oes para ler do teclado um n ́umero inteiro que representa uma quantidade x de
-#segundos. O programa deve imprimir na tela o n ́umero de horas, minutos e segundos contidos em x no formato HH:MM:SS.
-#Por exemplo, se x = 5000, ent ̃ao o resultado dever ́a ser 01:23:20, uma vez que 5000 segundos correspondem a 1 hora, 23
-#minutos e 20 segundos. Se x = 3600, o resultado dever ́a ser 01:00:00 (exatamente uma hora). Um exemplo  ́e mostrado a
-#seguir.
-#Entrada lida com o comando input:
-#3600
-#Sa ́ıda correspondente `a entrada:
-#01:00:00
+'''
+Faca um programa contendo instrucoes para ler do teclado um n ́umero inteiro que representa uma quantidade x de
+segundos. O programa deve imprimir na tela o n ́umero de horas, minutos e segundos contidos em x no formato HH:MM:SS.
+Por exemplo, se x = 5000, entao o resultado dever ́a ser 01:23:20, uma vez que 5000 segundos correspondem a 1 hora, 23
+minutos e 20 segundos. Se x = 3600, o resultado dever ́a ser 01:00:00 (exatamente uma hora). Um exemplo  ́e mostrado a
+seguir.
+Entrada lida com o comando input:
+3600
+Sa ́ıda correspondente a entrada:
+01:00:00
 '''
 
 x = int(input())
