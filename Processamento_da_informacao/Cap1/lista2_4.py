97c88d526cfa1909e440bf158e40e9f36e491544
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2024-11-19 16:10:56 -0300

    Update lista2_4.py

--- Processamento_da_informacao/Cap1/lista2_4.py
+++ Processamento_da_informacao/Cap1/lista2_4.py
@@ -1,3 +1,17 @@
+'''
+No Brasil a moeda e o Real (R$) e a Casa da Moeda atualmente imprime cedulas nos valores R$ 2, R$ 5, R$ 10, R$ 20,
+R$ 50, R$ 100 e R$ 200. Suponha que em um paıs peculiar, a moeda corrente e a JabuticabaCoins (OZ$), e que a Casa
+da Moeda desse paıs imprime cedulas dessa moeda nos seguinte valores: 1, 7, 8, 13, 33.
+Faca um programa que leia um numero inteiro e calcule o menor numero de cedulas de JabuticabaCoins no qual o
+valor lido pode ser decomposto. Em outras palavras, dado um valor em JabuticabaCoins (numero inteiro, lido teclado),
+seu programa deve imprimir – para cada valor de cedula existente – a quantidade mınima de cedulas necessarias para formar
+esse valor lido do teclado. Veja um exemplo de entrada e saıda abaixo.
+Fazer um modulo (funcao) chamado calculaDinheiro que recebe como parametros um valor inteiro qualquer e o valor
+de cada um dos tipos de cedula (1, 7, 8, 13, 33). Essa funcao deve retornar – para cada valor de cedula existente – a
+quantidade mınima de cedulas de JabuticabaCoins necessaria para formar esse valor.
+Atencao: o uso de funcoes e obrigatorio, a sua nao utilizacao implica em anulacao da questao.
+
+'''
 def entrada():
     global total
     total = int(input())
