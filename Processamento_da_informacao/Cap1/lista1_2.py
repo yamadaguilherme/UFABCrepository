e64e538096d22753dddfd31aaf74acfd71f44060
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2024-11-19 15:38:39 -0300

    Update lista1_2.py

--- Processamento_da_informacao/Cap1/lista1_2.py
+++ Processamento_da_informacao/Cap1/lista1_2.py
@@ -1,12 +1,13 @@
-#Fa ̧ca um programa para calcular e imprimir o valor da express ̃ao (f ́ormula) abaixo, a partir das vari ́aveis reais D e I. As
-#vari ́aveis D e I devem ser lidas do teclado, nessa ordem.
-#K = (raiz(D^2 + I^2))/5DI
-#Mostrar o resultado, com trˆes casas decimais, conforme exemplo a seguir:
-#Entradas:
-#8.400
-#5.300
-#Sa ́ıda:
-#K = 0.045
+'''
+Faca um programa para calcular e imprimir o valor da expressao (formula) abaixo, a partir das vari ́aveis reais D e I. As
+variaveis D e I devem ser lidas do teclado, nessa ordem.
+K = (raiz(D^2 + I^2))/5DI
+Mostrar o resultado, com tres casas decimais, conforme exemplo a seguir:
+Entradas:
+8.400
+5.300
+Saıda:
+K = 0.045
 
 D = float(input())
 I = float(input())
